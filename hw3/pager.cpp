158d887feb4098a0c584721adf4095e4922daa34
Jeff Gensler <jgensler8@gmail.com>
2014-10-19 08:28:20 -0500

    last minute analysis

--- hw3/pager.cpp
+++ hw3/pager.cpp
@@ -10,23 +10,16 @@
 #define PAGETABLESIZE 256
 #define NPROCESSES 256
 
-
-
-
-//****************
-
-
-// graph
-// page faults vs finite memory
-
-
-// ****************
-
-
-
 // can be run with both
 #define RUN_UNLIMITED 1
 #define RUN_FIFO 1
+
+//use to print human readable output
+//#define OUTPUT_HUMAN
+//use to generate .csv data
+//#define OUTPUT_CALC
+//user to generate data for graphs
+#define OUTPUT_GRAPHS
 
 using namespace std;
 
@@ -39,7 +32,7 @@ typedef struct {
 	int frame;
 	int pageHits;
 	int pageMisses;
-	//int timesSwapped
+	unsigned long int timesSwapped;
 	//int timePeriodSwapped? //relative to other pages
 } PageTableEntry;
 
@@ -62,6 +55,7 @@ void initPageTable(PageTableEntry table[PAGETABLESIZE][NPROCESSES])
       table[i][j].frame = -1;
       table[i][j].pageHits = 0;
       table[i][j].pageMisses = 0;
+      table[i][j].timesSwapped = 0;
     }
   }
 }
@@ -97,19 +91,76 @@ bool parseArgs(int argc, char** argv, char** filename, unsigned long int *number
 	}
 	
 	//get the number of references, if applicable argv[2]
-	if(argc >= 3) *numberReferences = atoi(argv[2]);
+	if(argc >= 3)
+	{
+	  *numberReferences = atoi(argv[2]);
+	  #ifdef OUTPUT_HUMAN
+	  cout << "ARG: NumberReferences: " << *numberReferences << endl;
+	  #endif
+	}
 	//get the frame table size, if applicable argv[3]
-	if(argc >= 4) *frameTableSize = atoi(argv[3]);
+	if(argc >= 4)
+	{
+	  *frameTableSize = atoi(argv[3]);
+	  #ifdef OUTPUT_HUMAN
+	  cout << "ARG: FrameTableSize:   " << *frameTableSize << endl << flush;
+	  #endif
+	}
 
 	return true;
 }
 
-void printStats(double pageHits, double pageMisses)
+void printStats(double pageHits, double pageMisses, char* filename, unsigned long int frameTableSize)
 {
   double total = pageHits + pageMisses;
+  #ifdef OUTPUT_HUMAN
   cout  << fixed
         << "PAGE HITS:   " << pageHits << "\t\t" << pageHits / total << endl
         << "PAGE MISSES: " << pageMisses << "\t\t" << pageMisses / total << endl << flush;
+  #endif
+  
+  #ifdef OUTPUT_GRAPHS
+  cout  << fixed
+        << filename << ","
+        << frameTableSize << ","
+        << pageHits / total << ","
+        << pageMisses / total <<  ",";
+  #endif
+}
+
+void printPageTableAdvanced()
+{
+  unsigned long int highestTimesSwapped, lowestTimesSwapped;
+  
+  double totalTimesSwapped = 0, nzrTimesSwapped = 0, nzrCount = 0;
+  
+  for(int pid = 0; pid < PAGETABLESIZE; ++pid)
+  {
+    for(int page = 0; page < NPROCESSES; ++page)
+    {
+      if(pageTables[pid][page].timesSwapped < lowestTimesSwapped) lowestTimesSwapped = pageTables[pid][page].timesSwapped;
+      
+      if(pageTables[pid][page].timesSwapped > highestTimesSwapped) highestTimesSwapped = pageTables[pid][page].timesSwapped;
+      
+      totalTimesSwapped += pageTables[pid][page].timesSwapped;
+      if(pageTables[pid][page].timesSwapped > 256)
+      {
+        nzrTimesSwapped += pageTables[pid][page].timesSwapped;
+        nzrCount += 1;
+      }
+    }
+  }
+  
+  #ifdef OUTPUT_HUMAN
+  cout  << fixed
+        << "HIGHEST TIMES SWAPPED: " << highestTimesSwapped << endl
+        << "LOWEST  TIMES SWAPPED: " << lowestTimesSwapped << endl
+        << "AVG ALL TIMES SWAPPED: " << totalTimesSwapped / (PAGETABLESIZE * NPROCESSES) << endl
+        << "AVG >256 TIMES SWAPPED: " << nzrTimesSwapped / nzrCount << endl << flush;
+  #endif
+  #ifdef OUTPUT_GRAPHS
+  cout << totalTimesSwapped / (PAGETABLESIZE * NPROCESSES) << ",";
+  #endif
 }
 
 void loadPage(FrameTableEntry* frameTable, int tableSize, int *top, int pid, int page)
@@ -118,6 +169,7 @@ void loadPage(FrameTableEntry* frameTable, int tableSize, int *top, int pid, int
   {
    //go out and adjust the thing we are about to overwrite
    pageTables[ frameTable[*top].pid ][ frameTable[*top].page ].frame = -1;
+   pageTables[ frameTable[*top].pid ][ frameTable[*top].page ].timesSwapped += 1;
   }
   
   //load the new page in
@@ -131,7 +183,7 @@ void loadPage(FrameTableEntry* frameTable, int tableSize, int *top, int pid, int
   if(*top == tableSize) *top = 0;
 }
 
-void simulate_unlimited(MemoryAccess* memblock, int numBlocks, FrameTableEntry* frameTable, unsigned long int frameTableSize, unsigned long int numberReferences)
+void simulate_unlimited(MemoryAccess* memblock, int numBlocks, FrameTableEntry* frameTable, unsigned long int frameTableSize, unsigned long int numberReferences, char* filename)
 {
  double pageHits = 0, pageMisses = 0;
  for( int i = 0; i < numberReferences; ++i)
@@ -153,10 +205,10 @@ void simulate_unlimited(MemoryAccess* memblock, int numBlocks, FrameTableEntry* 
      pageTables[ memblock[i].pid ][ memblock[i].page ].pageHits++;
    }
   }
-  printStats(pageHits, pageMisses);
+  printStats(pageHits, pageMisses, filename, frameTableSize);
 }
 
-void simulate_FIFO(MemoryAccess* memblock, int numBlocks, FrameTableEntry* frameTable, unsigned long int frameTableSize, unsigned long int numberReferences)
+void simulate_FIFO(MemoryAccess* memblock, int numBlocks, FrameTableEntry* frameTable, unsigned long int frameTableSize, unsigned long int numberReferences, char* filename)
 {
   double pageHits = 0, pageMisses = 0;
   int topStack = 0;
@@ -178,20 +230,39 @@ void simulate_FIFO(MemoryAccess* memblock, int numBlocks, FrameTableEntry* frame
       pageTables[ memblock[i].pid ][ memblock[i].page ].pageHits++;
     }
    }
-   printStats(pageHits, pageMisses);
+   printStats(pageHits, pageMisses, filename, frameTableSize);
 }
+
+#ifdef OUTPUT_CALC
+void generateCSV(MemoryAccess* memblock, int numBlocks, FrameTableEntry* frameTable, unsigned long int frameTableSize, unsigned long int numberReferences, char* filename)
+{
+  cout << "PID,PAGE,HITS,MISSES,TIMESSWAPPED" << endl;
+  for(int pid = 0; pid < PAGETABLESIZE; ++pid)
+  {
+    for(int page = 0; page < NPROCESSES; ++page)
+    {
+      cout << pid << "," << page << "," << pageTables[pid][page].pageHits << "," << pageTables[pid][page].pageMisses << "," << pageTables[pid][page].timesSwapped << endl;
+    }
+    cout << flush;
+  }
+}
+#endif
 
 int main(int argc, char** argv)
 {
 	char* filename = NULL;
 	unsigned long int numberReferences = -1, frameTableSize = FRAMETABLESIZE;
 
+  #ifdef OUTPUT_HUMAN
 	cout << "Jeff Gensler, jgensl2@uic.edu, jgensler -- CS account name" << endl << flush;
-
+  #endif
+  
 	//store informations from the command line
 	if(!parseArgs(argc, argv, &filename, &numberReferences, &frameTableSize))
 	{
+	  #ifdef OUTPUT_HUMAN
 		cout << "Invalid command line args" << endl << flush;
+		#endif
 		return -1;
 	}
 	
@@ -210,23 +281,40 @@ int main(int argc, char** argv)
 	fclose(file);
 	
 	//now to record the page hits and misses
+	#ifdef OUTPUT_HUMAN
   cout << "===== " << filename << " =====" << endl << flush;
+	#endif
 	
 	#ifdef RUN_FIFO
+	#ifdef OUTPUT_HUMAN
 	cout << "== SIMULATING FIFO ==" << endl << flush;
+	#endif
   initPageTable(pageTables);
   initFrameTable(frametable, frameTableSize);
-	simulate_FIFO(memblock, numBlocks, frametable, frameTableSize, numberReferences);
+	simulate_FIFO(memblock, numBlocks, frametable, frameTableSize, numberReferences, filename);
+	printPageTableAdvanced();
+	#ifdef OUTPUT_GRAPHS
+	cout << "FIFO" << endl << flush;
+	#endif
+	#ifdef OUTPUT_CALC
+	generateCSV(memblock, numBlocks, frametable, frameTableSize, numberReferences, filename);
+	#endif
 	#endif
 	#ifdef RUN_UNLIMITED
+	#ifdef OUTPUT_HUMAN
 	cout << "== SIMULATING UNLIMITED ==" << endl << flush;
+	#endif
   initPageTable(pageTables);
   initFrameTable(frametable, frameTableSize);
-	simulate_unlimited(memblock, numBlocks, frametable, frameTableSize, numberReferences);
+	simulate_unlimited(memblock, numBlocks, frametable, frameTableSize, numberReferences, filename);
+	#ifdef OUTPUT_GRAPHS
+	cout << "0,UNLIMITED" << endl << flush;
+	#endif
+	#ifdef OUTPUT_CALC
+	generateCSV(memblock, numBlocks, frametable, frameTableSize, numberReferences, filename);
+	#endif
 	#endif
 	
-	cout << endl << flush;
-
 	//free the memory allocated by mmap
 	munmap(memblock, filesize);
 	free(filename);
--- hw3/runAll.py
+++ hw3/runAll.py
@@ -1,7 +1,18 @@
 from subprocess import call
 
+print "PictureName,CacheSize,Hit %, Miss %, AVG Times Swapped,CacheType"
+
 def printAll(filenames):
   for x in filenames:
-    call(["./pager",x, "100000"])
+    with open("./csv/" + x + ".csv", "w") as outfile:
+      #call(["./pager",x], stdout=outfile)
+      call(["./pager",x,"10000000",str(256*1)])
+      call(["./pager",x,"10000000",str(256*3)])
+      call(["./pager",x,"10000000",str(256*6)])
+      call(["./pager",x,"10000000",str(256*12)])
+      call(["./pager",x,"10000000",str(256*25)])
+      call(["./pager",x,"10000000",str(256*50)])
+      call(["./pager",x,"10000000",str(256*100)])
     
-printAll( ["VRUPL_Logo.raw", "LUG_Newbies.raw", "Esher.raw", "calibration.raw", "diver.raw", "JohnBell.raw", "diver_resize.raw", "light_drops.raw", "GrandeJatte.raw"])
+#printAll( ["VRUPL_Logo.raw", "LUG_Newbies.raw", "Esher.raw", "calibration.raw", "diver.raw", "JohnBell.raw", "diver_resize.raw", "light_drops.raw", "GrandeJatte.raw"])
+printAll( ["VRUPL_Logo.raw", "LUG_Newbies.raw", "light_drops.raw", "GrandeJatte.raw"])
